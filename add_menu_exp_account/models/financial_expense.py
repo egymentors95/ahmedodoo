f35130c812a400900b62e1c1fc7d99275c020133
mohamed <ibs.developer1@gmail.com>
2026-06-15 11:54:48 +0300

    /

--- add_menu_exp_account/models/financial_expense.py
+++ add_menu_exp_account/models/financial_expense.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
+from odoo.tools.translate import _
 from odoo.exceptions import UserError, AccessError
 from datetime import date
 
